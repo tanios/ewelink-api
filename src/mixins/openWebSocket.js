db21563c0ea6045d3bb7dd10449562a742cbefb1
Guillaume Laliberté <g.laliberte-c@hotmail.ca>
2023-03-30 17:20:34 -0400

    websocket reconnection

--- src/mixins/openWebSocket.js
+++ src/mixins/openWebSocket.js
@@ -1,51 +1,81 @@
+const fetch = require('node-fetch');
 const W3CWebSocket = require('websocket').w3cwebsocket;
 const WebSocketAsPromised = require('websocket-as-promised');
 
 const wssLoginPayload = require('../payloads/wssLoginPayload');
+const errors = require('../data/errors');
 
 module.exports = {
-  /**
-   * Open a socket connection to eWeLink
-   * and execute callback function with server message as argument
-   *
-   * @param callback
-   * @param heartbeat
-   * @returns {Promise<WebSocketAsPromised>}
-   */
-  async openWebSocket(callback, ...{ heartbeat = 120000 }) {
-    const payloadLogin = wssLoginPayload({
-      at: this.at,
-      apiKey: this.apiKey,
-      appid: this.APP_ID,
-    });
+    /**
+     * Open a socket connection to eWeLink
+     * and execute callback function with server message as argument
+     *
+     * @param callback
+     * @param heartbeat
+     * @returns {Promise<WebSocketAsPromised>}
+     */
+    async openWebSocket(callback, ...{ heartbeat = 10000 }) {
+        if (this.activeWebSocket === true)
+            return console.error('WebSocket already open');
+        const dispatch = await this.getWebSocketServer();
+        const WSS_URL = `wss://${dispatch.domain}:${dispatch.port}/api/ws`;
 
-    const wsp = new WebSocketAsPromised(this.getApiWebSocket(), {
-      createWebSocket: wss => new W3CWebSocket(wss),
-    });
+        let payloadLogin = wssLoginPayload({
+            at: this.at,
+            apiKey: this.apiKey,
+            appid: this.APP_ID,
+        });
 
-    wsp.onMessage.addListener(message => {
-      try {
-        const data = JSON.parse(message);
-        callback(data);
-      } catch (error) {
-        callback(message);
-      }
-    });
+        this.wsp = new WebSocketAsPromised(WSS_URL, {
+            createWebSocket: (wss) => new W3CWebSocket(wss),
+        });
 
-    await wsp.open();
-    await wsp.send(payloadLogin);
+        this.wsp.onMessage.addListener((message) => {
+            this.activeWebSocket = true;
+            try {
+                const data = JSON.parse(message);
+                callback(data);
+            } catch (error) {
+                callback(message);
+            }
+        });
 
-    setInterval(async () => {
+        await this.wsp.open();
+        await this.wsp.send(payloadLogin);
 
-        try {
+        const interval = setInterval(async () => {
+            try {
+                await this.wsp.send('ping');
+            } catch (error) {
+                this.activeWebSocket = false;
+                console.error(`openWebSocket.js: ${error}`);
+                console.log(`openWebSocket.js: Reconnecting...`);
+                const auth = await this.getCredentials();
+                payloadLogin = wssLoginPayload({
+                    at: auth.at,
+                    apiKey: auth.user.apikey,
+                    appid: this.APP_ID,
+                });
+                await this.wsp.open();
+                await this.wsp.send(payloadLogin);
+            }
+        }, heartbeat);
 
-            await wsp.send('ping');
+        this.wsp.onClose.addListener(() => {
+            console.log(`openWebSocket.js: Connection closed`);
+            clearInterval(interval);
+            this.activeWebSocket = false;
+            this.wsp = undefined;
+        });
+    },
+
+    async getWebSocketServer() {
+        const requestUrl = this.getDispatchServiceUrl();
+        const request = await fetch(`${requestUrl}/dispatch/app`);
+        if (!request.ok) {
+            throw new Error(`[${request.status}] ${errors[request.status]}`);
         }
-        catch( err ) {
 
-        }
-    }, heartbeat);
-
-    return wsp;
-  },
+        return request.json();
+    },
 };
